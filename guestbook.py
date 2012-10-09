e340717c54cb6b8e9a7d0878068ec381d6035962
fredsa@google.com <fredsa@google.com@99225164-8649-0410-878b-2ba91e509939>
2012-10-09 23:29:16 +0000

    - Use guestbook entity group for consistency in High Replication datastore - db -> ndb
    git-svn-id: http://google-app-engine-samples.googlecode.com/svn/trunk/python27/guestbook@163 99225164-8649-0410-878b-2ba91e509939

--- guestbook.py
+++ guestbook.py
@@ -3,20 +3,28 @@ import os
 import webapp2
 
 from google.appengine.api import users
-from google.appengine.ext import db
+from google.appengine.ext import ndb
+
+
+# We set a parent key on the 'Greetings' to ensure that they are all in the same
+# entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
+
+def guestbook_key(guestbook_name='default_guestbook'):
+  return ndb.Key('Guestbook', guestbook_name)
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
 
-class Greeting(db.Model):
-    author = db.UserProperty()
-    content = db.StringProperty(multiline=True)
-    date = db.DateTimeProperty(auto_now_add=True)
+class Greeting(ndb.Model):
+    author = ndb.UserProperty()
+    content = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        greetings_query = Greeting.all().order('-date')
+        greetings_query = Greeting.query(ancestor=guestbook_key()).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
 
         if users.get_current_user():
@@ -33,7 +41,7 @@ class MainPage(webapp2.RequestHandler):
 
 class Guestbook(webapp2.RequestHandler):
     def post(self):
-        greeting = Greeting()
+        greeting = Greeting(parent=guestbook_key())
 
         if users.get_current_user():
             greeting.author = users.get_current_user()
