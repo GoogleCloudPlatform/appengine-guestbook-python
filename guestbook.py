306bfbdb74c6814f41ebf4b314d158bed64fc384
Fred Sauer <fredsa@google.com>
2013-04-24 17:20:47 -0700

    use ancestor queries for strong consistency
    Reviewed in https://codereview.appspot.com/8671051/

--- guestbook.py
+++ guestbook.py
@@ -1,10 +1,15 @@
-import jinja2
 import os
-import webapp2
+import urllib
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
 # We set a parent key on the 'Greetings' to ensure that they are all in the same
 # entity group. Queries across the single entity group will be consistent.
@@ -13,18 +18,19 @@ from google.appengine.ext import ndb
 def guestbook_key(guestbook_name='default_guestbook'):
     return ndb.Key('Guestbook', guestbook_name)
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'])
 
 class Greeting(ndb.Model):
     author = ndb.UserProperty()
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+
 class MainPage(webapp2.RequestHandler):
+
     def get(self):
-        greetings_query = Greeting.query(ancestor=guestbook_key()).order(-Greeting.date)
+        guestbook_name = self.request.get('guestbook_name')
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
 
         if users.get_current_user():
@@ -34,21 +40,35 @@ class MainPage(webapp2.RequestHandler):
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
 
-        template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render(greetings=greetings,
-                                                url=url,
-                                                url_linktext=url_linktext))
+        template_values = {
+            'greetings': greetings,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
 
 class Guestbook(webapp2.RequestHandler):
+
     def post(self):
-        greeting = Greeting(parent=guestbook_key())
+        # We set the same parent key on the 'Greeting' to ensure each Greeting
+        # is in the same entity group. Queries across the single entity group
+        # will be consistent. However, the write rate to a single entity group
+        # should be limited to ~1/second.
+        guestbook_name = self.request.get('guestbook_name')
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
 
         if users.get_current_user():
             greeting.author = users.get_current_user()
 
         greeting.content = self.request.get('content')
         greeting.put()
-        self.redirect('/')
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
