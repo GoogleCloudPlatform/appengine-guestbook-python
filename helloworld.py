c72106f59178e226c71f9aba269e6805d6a772b7
Fred Sauer <fredsa@google.com>
2013-05-28 15:25:27 -0700

    sync part2-usingusers branch with documentation https://developers.google.com/appengine/docs/python…
    Reviewed in https://codereview.appspot.com/9834045/

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+from google.appengine.api import users
+
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+
+        user = users.get_current_user()
+
+
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('Hello, ' + user.nickname())
+
+
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
