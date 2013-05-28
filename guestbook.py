e7567f8901f56e7cdf216018f77520f7fbc75c47
Fred Sauer <fredsa@google.com>
2013-05-28 14:36:39 -0700

    sync part3-handlingforms branch with documentation https://developers.google.com/appengine/docs/python/gettingstartedpython27/handlingforms

---
+++ guestbook.py
@@ -0,0 +1,37 @@
+import cgi
+
+from google.appengine.api import users
+
+import webapp2
+
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+"""
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.write(MAIN_PAGE_HTML)
+
+
+class Guestbook(webapp2.RequestHandler):
+
+    def post(self):
+        self.response.write('<html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('content')))
+        self.response.write('</pre></body></html>')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/sign', Guestbook),
+], debug=True)
