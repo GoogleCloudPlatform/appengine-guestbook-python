c14cfffe0315281499feef671516734af86191c8
Fred Sauer <fredsa@google.com>
2013-06-27 11:21:51 -0700

    simply 'Hello, World!' ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=48637035

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, webapp2 World!')
+        self.response.write('Hello, World!')
 
 
 application = webapp2.WSGIApplication([
