f51c873ddfdde2ec1adc9f27241bd37c9e0141b4
johndmulhausen <johndylan@google.com>
2014-03-26 17:59:00 -0700

    Update guestbook.py

--- guestbook.py
+++ guestbook.py
@@ -1,9 +1,6 @@
 import cgi
-
 from google.appengine.api import users
-
 import webapp2
-
 
 MAIN_PAGE_HTML = """\
 <html>
@@ -16,20 +13,15 @@ MAIN_PAGE_HTML = """\
 </html>
 """
 
-
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
         self.response.write(MAIN_PAGE_HTML)
 
-
 class Guestbook(webapp2.RequestHandler):
-
     def post(self):
         self.response.write('<html><body>You wrote:<pre>')
         self.response.write(cgi.escape(self.request.get('content')))
         self.response.write('</pre></body></html>')
-
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
