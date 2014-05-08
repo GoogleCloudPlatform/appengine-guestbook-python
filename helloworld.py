a1abf4084ba9d0d8f6e428b81bb5286dcee2f66b
johndmulhausen <johndylan@google.com>
2014-05-08 16:26:49 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,6 @@
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
